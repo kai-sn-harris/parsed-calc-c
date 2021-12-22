9d6318fd2b25fc284a89d6867a643f7b20917815
Kai Harris <m3sv.is.me@gmail.com>
2021-12-22 20:16:13 +1100

    added new line on error msg

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[]) {
     if(argc == 1) {
-        printf("Error: must supply equation to be solved");
+        printf("Error: must supply equation to be solved\n");
         exit(EXIT_FAILURE);
     }
 
